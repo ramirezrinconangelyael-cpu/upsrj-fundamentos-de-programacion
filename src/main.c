b0f90238b8c2ec50661cfe62020f12ff7b441395
ramirezrinconangelyael-cpu <ramirezrinconangelyael@gmail.com>
2026-01-19 13:01:37 -0600

    ENTREGA: AREA & VOLUMEN

--- src/main.c
+++ src/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
-int main(int argc, char *argv)
+int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    double side;
+    double area, volume; 
+
+    printf("Enter the side length: ");
+    scanf("%lf", &side);
+
+    if(side < 0){
+
+        printf("Error: the side length must be a postivie number.\n");
+
+        return 1; 
+
+    }
+
+    area = side * side; 
+    volume = side * side * side;
+
+    printf("Square area: %.2f\n", area); 
+    printf("Cube area: %.2f\n", volume);
+
+    return 0; 
+    
 }
+
+
